3419b56deb62404c3d6b3bf69ce43eafa0da706d
Will Deacon <will.deacon@arm.com>
2018-04-26 11:34:15 +0100

    locking/barriers: Introduce smp_cond_load_relaxed() and atomic_cond_read_relaxed()
    Whilst we currently provide smp_cond_load_acquire() and
    atomic_cond_read_acquire(), there are cases where the ACQUIRE semantics are
    not required because of a subsequent fence or release operation once the
    conditional loop has exited.
    
    This patch adds relaxed versions of the conditional spinning primitives
    to avoid unnecessary barrier overhead on architectures such as arm64.
    
    Change-Id: Ic8f5b85c8bfdf5dcb54ff3c1ac59ccc905882a30
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Waiman Long <longman@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: boqun.feng@gmail.com
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: paulmck@linux.vnet.ibm.com
    Link: http://lkml.kernel.org/r/1524738868-31318-2-git-send-email-will.deacon@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/linux/atomic.h
+++ include/linux/atomic.h
@@ -627,6 +627,7 @@ static inline int atomic_dec_if_positive(atomic_t *v)
 }
 #endif
 
+#define atomic_cond_read_relaxed(v, c)	smp_cond_load_relaxed(&(v)->counter, (c))
 #define atomic_cond_read_acquire(v, c)	smp_cond_load_acquire(&(v)->counter, (c))
 
 #define atomic_cond_read_relaxed(v, c)	smp_cond_load_relaxed(&(v)->counter, (c))
