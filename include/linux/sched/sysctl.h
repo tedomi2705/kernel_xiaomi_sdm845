49e6d28067c3d4929f14731089099538e3d81c4a
kanonifyX <kanonify01@gmail.com>
2024-12-11 07:46:13 +0700

    sched: core: Add userspace affinity threads
    sched/core: Fix userspace affining threads incorrectly
    Certain userspace applications, to achieve max performance, affines its
    threads to cpus that run the fastest. This is not always the
    correct strategy. For e.g. in certain architectures all the
    cores have the same max freq but few of them have a bigger
    cache. Affining to the cpus that have bigger cache is advantageous
    but such an application would end up affining them to all the cores.
    Similarly if an architecture has just one cpu that runs at max freq,
    it ends up crowding all its thread on that single core, which is
    detrimental for performance.
    
    To address this issue, we need to detect a suspicious looking affinity
    request from userspace and check if it links in a particular library.
    The latter can easily be detected by traversing executable vm areas
    that map a file and checking for that library name.
    When such a affinity request is found, change it to use a proper
    affinity. The suspicious affinity request, the proper affinity request
    and the library name can be configured by the userspace.
    
    Change-Id: I6bb8c310ca54c03261cc721f28dfd6023ab5591a
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    
    sched/core: Fix use after free issue in is_sched_lib_based_app()
    is_sched_lib_based_app() function introduced by 'commit ("sched/core: fix userspace affining threads incorrectly")' traverses
    all the executable VMA regions of a task for which the affinity change
    is requested by the userspace. The mm->mmap_sem lock is acquired to
    lock the VMA regions, however the task mm itself can go away when
    the task is exited. The get_task_struct() does not prevent this from
    happening. Add protection by incrementing task's mm reference count.
    
    Change-Id: I39d835a8d7d53d9b9eca90baf73d3fcfad9d164b
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    
    sched: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: I3842c589378ce5bcc54e4c35ebcbfbdfa1f88a8f
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    
    backport to 4.9
    Signed-off-by: KanonifyX <kanonify01@gmail.com>
    Signed-off-by: kanonifyX <kanonify01@gmail.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -143,4 +143,8 @@ extern int sched_little_cluster_coloc_fmin_khz_handler(struct ctl_table *table,
 					int write, void __user *buffer,
 					size_t *lenp, loff_t *ppos);
 #endif
+#define LIB_PATH_LENGTH 512
+extern char sched_lib_name[LIB_PATH_LENGTH];
+extern unsigned int sched_lib_mask_force;
+extern bool is_sched_lib_based_app(pid_t pid);
 #endif /* _SCHED_SYSCTL_H */
