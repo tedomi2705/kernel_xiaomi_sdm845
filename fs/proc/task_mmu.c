5e47f60ee49bb2ed6068f537d9444ad8d05b4ee2
kanonifyX <kanonify01@gmail.com>
2025-01-20 21:21:12 +0700

    /proc/pid/maps: fix warning mixing declarations and code is a C99 extension [-Wdeclaration-after-statement]
    Signed-off-by: KanonifyX <kanonify01@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -376,13 +376,14 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
+	struct dentry *dentry = NULL;
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-        struct dentry *dentry = file->f_path.dentry;
+        dentry = file->f_path.dentry;
         if (dentry) {
         	const char *path = (const char *)dentry->d_name.name; 
             	if (strstr(path, "lineage")) { 
