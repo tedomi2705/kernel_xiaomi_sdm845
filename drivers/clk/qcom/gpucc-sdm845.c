c412d457243e03ec3fc64549bd860f6a985d4539
Kaiyaa77 Yuu507 <kaiyaa@kaiyaa77.com>
2025-10-25 16:21:47 +0700

    arm64: dts: change max freq gpu to 844MHz

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -282,8 +282,8 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-	F(745000000, P_CRC_DIV,  1, 0, 0),
 	F(802000000, P_CRC_DIV,  1, 0, 0),
+	F(844000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -642,9 +642,9 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 		675000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 710000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_1] =
-		745000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
 		802000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
+		844000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
